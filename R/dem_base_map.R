1bdef122c3675565671983c8040bb3288f308cac
Adam Shand <adamshand22@gmail.com>
2026-04-09 11:47:36 +1000

    base map now optionally crops

--- R/dem_base_map.R
+++ R/dem_base_map.R
@@ -1,14 +1,14 @@
 #' Create the base map elements for a DEM map
 #'
 #' @param sr A SpatRaster object, generally produced by the dem_pre_processing() function.
-#' @param Resolution A numeric vector, either 30 or 100. This should match the resolution of the sr object provided.
 #' @param OutputPath A character vector that defines the path to the folder where the outputs should be saved
 #' @param FileName A character vector that uniquely identifies the outputs. Do not include a file type
+#' @param Zscale A numeric vector. Defines ratio between x and y spacing. Defaults to Resolution input (realistic). Decrease 
+#' Zscale to exagerate heights
+#' @param CropObj A sf or SpatVector object. This is optionally used to further crop the data to a specific location
 #' @param Reload Boolean. Do you want to try and reload a prior saved dataset with the same name? Defaults to TRUE.
 #' @param Overwrite Boolean. Do you want to overwrite the previously saved dataset with the same name? Defaults to FALSE
 #' @param Texture A character vector. Describes the colour palette to use. One of: “imhof1”, “imhof2”, “imhof3”, “imhof4”, “desert”, “bw”, “unicorn”.
-#' @param Zscale A numeric vector. Defines ratio between x and y spacing. Defaults to Resolution input (realistic). Decrease 
-#' Zscale to exagerate heights
 #' @param SeaLevel A numeric vector. Defines the elevation of sea level. Defaults to 0m. Decrease or increse to simulate
 #' various sea levels.
 #' 
@@ -19,36 +19,42 @@
 #' @examples
 #' \dontrun{ #dont run because function takes a long time
 #' 
+#' ross <- build_n3_region() |> 
+#'   filter(BasinOrZone == "Ross")
+#' 
 #' dem_cropped <- dem_base_map(
 #'   sr = my_spat_raster,
-#'   Resolution = 30,
 #'   OutputPath = "path/to/output folder/",
-#'   FileNmae = "my_file",
+#'   FileName = "my_file",
+#'   Zscale = 30,
+#'   CropObj = ross,
 #'   Reload = FALSE,
 #'   Overwrite = FALSE,
 #'   Texture = "Desert",
-#'   Zscale = 30,
 #'   SeaLevel = 0
 #' )
 #' }
 #' 
 dem_base_map <- function(
-  sr, Resolution, OutputPath, FileName, Reload = TRUE, 
-  Overwrite = TRUE, Texture = "Desert", Zscale = NULL, SeaLevel = 0){
-
+  sr, OutputPath, FileName, Zscale, CropObj = NULL, Reload = TRUE, 
+  Overwrite = TRUE, Texture = "Desert", SeaLevel = 0){
+  
   #check required arguments
-  if (any(missing(sr), missing(Resolution), missing(OutputPath), missing(FileName))){
-    stop("You must supply all arguments: 'sr', 'Resolution', 'OutputPath' and 'FileName'.")}
+  if (any(missing(sr), missing(OutputPath), missing(FileName), missing(Zscale))){
+    stop("You must supply all arguments: 'sr', 'OutputPath', 'FileName' and 'Zscale'.")}
 
   #check required arguement types
   if (!inherits(sr, "SpatRaster")){stop("The argument supplied to the 'sr' parameter must be of SpatRaster type.")}
-  if (!is.numeric(Resolution)){stop("The argument supplied to the 'Resolution' parameter must be of numeric type.")}
+  if (!is.numeric(Zscale)){stop("The argument supplied to the 'Zscale' parameter must be of numeric type.")}
   if (!is.character(OutputPath)){stop("The argument supplied to the 'OutputPath' parameter must be of character type.")}
   if (!is.character(FileName)){stop("The argument supplied to the 'FileName' parameter must be of numeric type.")}
 
   #check optional arguments
-  if (is.null(Zscale)){Zscale <- Resolution}
-  if (!is.numeric(Zscale)){stop("The argument supplied to the 'Zscale' parameter must be of numeric type.")}
+  if (!is.null(CropObj)){
+    if (!inherits(CropObj, "SpatVector") | !inherits(CropObj, "sf")){
+      stop("The object supplied it 'CropOb' is neither a SpatRaster or sf object.")
+    }
+  }
   if (!is.logical(Reload)){stop("The argument supplied to the 'Reload' parameter must be boolean (TRUE or FALSE).")}
   if (!is.logical(Overwrite)){stop("The argument supplied to the 'Overwrite' parameter must be boolean (TRUE or FALSE).")}
   if (!is.numeric(SeaLevel)){stop("The argument supplied to the 'SeaLevel' parameter must be of numeric type.")}
@@ -61,11 +67,11 @@ dem_base_map <- function(
   }
 
   #create full paths to where the matrix and array will be stored
-  array_path <- glue::glue("{OutputPath}/{FileName}_{Resolution}m_array")
-  matrix_path <- glue::glue("{OutputPath}/{FileName}_{Resolution}m_matrix")
+  array_path <- glue::glue("{OutputPath}/{FileName}_array")
+  matrix_path <- glue::glue("{OutputPath}/{FileName}_matrix")
 
   #reload files if requested and existing
-  if (Reload & all(file.eixsts(c(array_path, matrix_path)))){
+  if (Reload & all(file.exists(c(array_path, matrix_path)))){
 
     #read in
     area_array <- readRDS(file = array_path)
@@ -80,8 +86,28 @@ dem_base_map <- function(
   #if the files dont exist, or the user didnt request them. Build
   } else {
 
+    #if a crop object has been provide, further crop the data
+    if (!is.null(CropObj)){
+
+      #if the object is SpatVector, go straight to ext
+      if (inherits(CropObj, "SpatVector")){CropObj <- ext(CropObj)
+
+      #if the object is sf, get bbox, then convert to spatraster
+      } else {CropObj <- vect(st_as_sfc(st_bbox(CropObj)))}
+
+      #update the crs of the object
+      CropObj <- terra::project(CropObj, "EPSG:4326")
+
+      #crop the SpatRaster
+      sr <- trim(mask(sr, CropObj))
+      
+    } 
+
     #convert raster into a matrix
     area_matrix <- rayshader::raster_to_matrix(sr)
+
+    #check if na values are present in the matrix
+    if (any(is.na(area_matrix))){area_matrix <- zoo::na.approx(area_matrix, rule = 2)}
 
     #create a bathymetry version for water colours (remove everything above the sea level variable)
     area_bathymetry <- area_matrix
@@ -113,7 +139,7 @@ dem_base_map <- function(
 
     #combine core layers to form the area_array
     area_array <- area_matrix |> 
-      rayshader::sphere_shade(texture = texture, zscale = 1) |> 
+      rayshader::sphere_shade(texture = Texture, zscale = 1) |> 
       rayshader::add_shadow(ray_matrix, max_darken = 0.3) |> 
       rayshader::add_shadow(lam_matrix) |> 
       rayshader::add_shadow(amb_matrix) |>
@@ -124,7 +150,7 @@ dem_base_map <- function(
     if (!all(is.na(area_bathymetry))){#same logic as bathymetry section above
       area_array <- area_array |> 
         rayshader::add_overlay(
-          rayshader::generate_altitude_overlay(bath_array, area_matrix, sea_level, sea_level))
+          rayshader::generate_altitude_overlay(bath_array, area_matrix, SeaLevel, SeaLevel))
       print("Bathymetry maxtrix added.")
     }
 
@@ -137,6 +163,5 @@ dem_base_map <- function(
 
     #return and end
     return(base_map_objects)  
-  } 
+  }
 }
-
