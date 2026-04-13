01a20f2ccb8696fdb2b44aa20065d04ab975ada8
Adam Shand <adamshand22@gmail.com>
2026-04-13 14:11:24 +1000

    add river overlay function

--- R/dem_water_overlay.R
+++ R/dem_water_overlay.R
@@ -4,6 +4,7 @@
 #' @param Polygons An sf object that defines the polygon waterbodies to be put onto the map (e.g. lakes)
 #' @param Extent An sf object that defines the full map boundaries
 #' @param MapArray An array object created specifically by the 'dem_base_map()' function
+#' @param MapMatrix A matrix object created specifically by the 'dem_base_map()' function
 #' 
 #' 
 #' @returns The provided array object with waterbodies now embedded.
@@ -12,24 +13,27 @@
 #' @examples
 #' \dontrun{ #dont run because function takes a long time
 #' 
-#' ross_waters <- extract_watercourses("Ross", WaterAreas = TRUE, WaterLakes = TRUE, StreamOrder = 1
+#' ross_waters <- extract_watercourses("Ross", WaterAreas = TRUE, WaterLakes = TRUE, StreamOrder = 2)
 #' 
 #' map_ext <- ext(my_spat_raster)
 #' 
 #' map_array <- dem_base_map(...)
+#' map_matrix <- dem_base_map(...)
 #' 
 #' dem_cropped <- dem_water_overlay(
 #'   Lines = ross_waters,
 #'   Polygons = ross_waters,
 #'   Extent = map_ext,
-#'   MapArray = map_array
+#'   MapArray = map_array,
+#'   MapMatrix = map_matrix
 #' )
 #' }
 #' 
-dem_water_overlay <- function(Lines, Polygons, Extent, MapArray){
+dem_water_overlay <- function(Lines, Polygons, Extent, MapArray, MapMatrix){
   
   #all arguments are required
-  if (any(missing(Lines), missing(Extent), missing(MapArray))){stop("'Lines', 'Extent', and 'MapArray' arguments are required.")}
+  if (any(missing(Lines), missing(Extent), missing(MapArray), missing(MapMatrix))){
+    stop("'Lines', 'Extent', 'MapArray', and 'MapMatrix' arguments are required.")}
 
   #check types
   if (!inherits(Lines, "sf")){
@@ -45,6 +49,9 @@ dem_water_overlay <- function(Lines, Polygons, Extent, MapArray){
   }
   if (!(inherits(MapArray, "array") | inherits(MapArray, "rayimg"))){
     stop("The object supplied to 'MapArray' must be an array produced by the dem_pre_processing function.")
+  }
+  if (!(inherits(MapMatrix, "matrix") | inherits(MapArray, "array"))){
+    stop("The object supplied to 'MapMatrix' must be an matrix produced by the dem_pre_processing function.")
   }
 
   #update crs of sf objects
